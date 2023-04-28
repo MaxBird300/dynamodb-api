3c507ba65b1de8ff0a69b250b38ca335a80fcbbb
Max Bird <maxbird300@gmail.com>
2023-04-28 11:22:08 +0100

    updated version in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 
 setup(
     name='ddbwrapper',
-    version='1.2.1',
+    version='1.2.2',
     description='Wrapper to easily access DynamoDB table',
     long_description=readme,
     author='Max Bird',
